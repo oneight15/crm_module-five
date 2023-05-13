07b3e59450cd61fd617c436db1b2433343bb54e0
oneight15 <oneight1508@gmail.com>
2023-05-13 20:30:08 +0500

    feat: add solution lesson 2

---
+++ js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const title = document.querySelector('.modal__title');
+const identText = document.querySelector('.modal__ident-text');
+const identBtn = document.querySelector('.modal__ident-btn');
+const form = document.querySelector('.modal__form');
+const discountCheckbox = document.querySelector('.modal__input-checkbox');
+const discountInput = document.querySelector('.modal__input_disabled');
+const totalPrice = document.querySelector('.modal .total-price__sum');
