d0ff7f86b8095bff9dfe4b2c622a8fb133034d47
oneight15 <oneight1508@gmail.com>
2023-08-06 22:35:35 +0500

    feat: add fn openPic

--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@ import renderGoods from './modules/render.js';
 import formControl from './modules/form.js';
 import * as elem from './modules/elements.js';
 import * as controlModal from './modules/modal.js';
+import openPic from './modules/openPic.js';
 
 const init = () => {
   renderGoods(elem.goods);
   controlModal.stateModal();
   formControl();
+  openPic();
 };
 
 init();
---
+++ js/modules/openPic.js
@@ -0,0 +1,24 @@
+import * as elem from './elements.js';
+
+const openPic = () => {
+  const icon = document.querySelector('.table-icon_type_pic');
+  const iconPic = icon.dataset.pic;
+
+  elem.tableBody.addEventListener('click', e => {
+    const btnIcon = e.target.closest('.table-icon_type_pic');
+    const valueTop = (screen.height / 2) - 300;
+    const valueLeft = (screen.width / 2) - 300;
+
+    if (btnIcon) {
+      const win = open('about:blank', '', `
+        width=600,
+        height=600,
+        top=${(valueTop)},
+        left=${valueLeft}
+        `);
+      win.location.href = iconPic;
+    }
+  });
+};
+
+export default openPic;
